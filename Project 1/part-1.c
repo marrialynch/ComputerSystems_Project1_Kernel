af5d202a9a821f4cbeedeb1bdb0999a31604ebec
marrialynch <marilyn.intel@gmail.com>
2022-02-12 15:29:40 -0800

    quit not working

--- Project 1/part-1.c
+++ Project 1/part-1.c
@@ -5,8 +5,7 @@
 
 /* THE ONLY INCLUDE FILE */
 #include "sysdefs.h"
-#include <stdio.h>
-#include <stdlib.h>
+#define BUFFER_SIZE 201
 
 /* write these functions */
 
@@ -20,9 +19,9 @@ void exit(int err);
    Write new functions using the functions you just wrote
 */
 
-/* read one line from stdin (file descriptor 0) into a buffer: */
+
 // len: number of bytes to read
-// return: 0 for success
+// return: 1 for success
 int read(int fd, void *ptr, int len)
 {
    if (len < 1)
@@ -31,14 +30,22 @@ int read(int fd, void *ptr, int len)
    }
 
    syscall(__NR_read, fd, ptr, len);
-   // printf("%s \n", (char *)ptr);
    return 1;
 }
 
-char *readline()
+int strcmp(const char* str1, const char* str2) {
+   while(*str1 && (*str1 == *str2)) {
+      str1++;
+      str2++;
+   }
+   return *(const unsigned char*) str1 - *(const unsigned char*) str2;
+}
+
+/* read one line from stdin (file descriptor 0) into a buffer: */
+void readline(char* buffer)
 {
-   char *buffer = (char *)malloc(sizeof(char) * 201);
-   for (int i = 0; i < 201; i++)
+   
+   for (int i = 0; i < BUFFER_SIZE; i++)
    {
       read(0, &buffer[i], 1);
       if (buffer[i] == '\n')
@@ -47,17 +54,49 @@ char *readline()
          break;
       }
    }
-   return buffer;
 }
+
+int write(int fd, void *ptr, int len) {
+      if (len < 1)
+   {
+      return -1;
+   }
+   syscall(__NR_write, fd, ptr, len);
+   return 1;
+}
+
 /* print a string to stdout (file descriptor 1) */
+void print(char* buffer) {
+   for (int i = 0; i < BUFFER_SIZE; i++)
+   {
+      write(1, &buffer[i], 1);
+      if (buffer[i] == '\0')
+      {
+         break;
+      }
+   }
+}
+
+void exit(int err){
+   syscall(__NR_exit, err);
+}
 
 /* ---------- */
-
-int main(void)
-{
+void main (){
    /* your code here */
-   char *res = (char *)malloc(sizeof(char) * 3);
-   printf("hello, type: \n");
-   read(0, res, 1);
-   return 1;
+   char buffer[BUFFER_SIZE];
+
+   char* welcome_prompt = "hello, type lines of input, or 'quit'\n";
+   char* print_prompt = "you typed: ";
+   char* exit_str = "quit";
+
+   print(welcome_prompt);
+   while(1) {
+      readline(buffer);
+      if (strcmp(buffer, exit_str) == 0){
+         exit(1);
+      }
+      print(print_prompt);
+      print(buffer);
+   }
 }
